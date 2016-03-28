f136ae9e304f5548543f55f4aa726e9d0cc4c216
bergi <bergi@axolotlfarm.org>
2016-03-28 15:45:40 +0200

    use absolute-url module

--- index.js
+++ index.js
@@ -1,54 +1,7 @@
-var path = require('path')
-var url = require('url')
+var absoluteUrl = require('absolute-url')
 
 function init (options) {
-  options = options || {}
-
-  return function (req, res, next) {
-    req.absoluteUrl = function () {
-      var absoluteUrl = {
-        protocol: this.protocol,
-        hostname: this.hostname,
-        port: this.socket.address().port,
-        pathname: this.originalUrl || this.url
-      }
-
-      if (absoluteUrl.protocol === 'http' && req.socket.ssl) {
-        absoluteUrl.protocol += 's'
-      }
-
-      if (options.basePath) {
-        absoluteUrl.pathname = path.join(options.basePath, absoluteUrl.pathname)
-      }
-
-      // use proxy header fields?
-      if (req.app.get('trust proxy')) {
-        if ('x-forwarded-proto' in this.headers) {
-          absoluteUrl.protocol = this.headers['x-forwarded-proto']
-        }
-
-        if ('x-forwarded-host' in this.headers) {
-          var hostParts = this.headers['x-forwarded-host'].split(':')
-
-          absoluteUrl.hostname = hostParts.shift()
-          absoluteUrl.port = hostParts.shift()
-        }
-      }
-
-      // ignore port if default http(s) port
-      if (absoluteUrl.protocol === 'http' && absoluteUrl.port === 80) {
-        absoluteUrl.port = ''
-      }
-
-      if (absoluteUrl.protocol === 'https' && absoluteUrl.port === 443) {
-        absoluteUrl.port = ''
-      }
-
-      return url.format(absoluteUrl)
-    }
-
-    next()
-  }
+  return absoluteUrl(options)
 }
 
 function initLdapp () {
